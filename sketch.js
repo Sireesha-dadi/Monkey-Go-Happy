4177dd25e8ab4b5ae2673677231de20a2c269c2f
Sireesha-dadi <75783439+Sireesha-dadi@users.noreply.github.com>
2020-12-10 15:32:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,109 @@
+
+var monkey , monkey_running
+var ground;
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score=0, survivalTime=0;
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  createCanvas(400,400);
+  
+  monkey = createSprite(80,315,20,60);
+  monkey.addAnimation("running", monkey_running);
+  monkey.scale=0.1;
+  
+  ground = createSprite(400,350,900,10);
+  ground.velocityX=-6;
+  ground.x= ground.width/2;
+  
+  FoodGroup= new Group();
+  obstacleGroup= new Group();
+}
+
+
+function draw() {
+   background("pink");
+  //console.log(monkey.y);
+  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("score : "+score , 300,50);
+  
+  stroke("black");
+  textSize(20);
+  fill("black");
+  survivalTime=Math.ceil(frameCount/frameRate());
+  text("Survival Time : "+survivalTime, 70,50);
+  
+  
+  if(keyDown("space") && monkey.y>=314){
+    monkey.velocityY=-20;
+  }
+  
+  monkey.velocityY=monkey.velocityY+0.8;
+  
+  if (ground.x<0){
+    ground.x= ground.width/2;
+  }
+  
+  monkey.collide(ground);
+  
+  spawnBanana();
+  spawnObstacle();
+  
+  drawSprites();
+  
+}
+
+function spawnBanana(){
+  
+  if(frameCount % 80 ===0){
+    
+    banana= createSprite(400,300,40,10);
+    banana.velocityX=-4;
+    banana.addImage(bananaImage);
+    banana.y=Math.round(random(120,200));
+    banana.lifetime=100;
+    banana.scale=0.1;
+    
+    banana.depth=monkey.depth;
+    monkey.depth= monkey.depth+1;
+  
+    FoodGroup.add(banana);
+  }
+  
+}
+
+function spawnObstacle(){
+  
+  if(frameCount % 300 ===0){
+    
+    obstacle= createSprite(400,330,40,10);
+    obstacle.velocityX=-4;
+    obstacle.addImage(obstacleImage);
+    obstacle.lifetime=100;
+    obstacle.scale=0.1;
+    
+    obstacleGroup.add(obstacle);
+  }
+  
+}
+
+
+
+
+
+
